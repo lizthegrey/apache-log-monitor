c11c1fdd1e1a621755743c60807ed73d8ac73a75
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2015-02-22 00:50:20 -0500

    add console integration.

--- cmd/monitor_main.go
+++ cmd/monitor_main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/lizthegrey/apache-log-monitor/monitor"
+	"log"
+	"os"
 )
 
 var filename = flag.String("file", "/var/log/apache2/access.log", "The filename of the W3C formatted logfile to parse.")
@@ -11,35 +12,54 @@ var pollIntervalMs = flag.Int64("poll_interval_ms", 250, "The number of millisec
 
 func main() {
 	flag.Parse()
-	lines := make(chan string)
+	log.SetOutput(os.Stderr)
+
+	// Buffering limit of 5 picked out of hat. Definitely needs to be >1 but value should be experimentally determined.
+	lines := make(chan string, 5)
+	consoleStatus := make(chan string)
+	consoleLog := make(chan string, 5)
+	terminate := make(chan bool)
+
 	file, err := monitor.TailFile(*filename, lines, *pollIntervalMs)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	terminate := make(chan bool)
 	go func() {
 		err := file.ContinuousRead()
-		fmt.Println(err)
+		log.Println(err)
 		// Signal cleanup if main read loop is over.
 		file.Close()
 	}()
-	go func() {
-		for {
-			line, more := <-lines
-			if !more {
-				terminate <- true
-				return
-			}
 
+	go func() {
+		for line := range lines {
 			result, err := monitor.LogParse(line)
 			if err == nil {
-				fmt.Println(result.Url)
+				// TODO(lizf): Send the result object to all registered statistics modules,
+				//             then add extra indirection layer for stats modules to write to console/status.
+				consoleLog <- result.Url.String()
 			} else {
-				fmt.Println(err)
+				consoleLog <- err.Error()
 			}
 		}
+		terminate <- true
 	}()
-	// trigger console functionality here
-	<-terminate
+
+	console, err := monitor.NewConsole(consoleStatus, consoleLog)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	go func() {
+		<-terminate
+		console.Interrupt()
+	}()
+
+	defer console.Cleanup()
+	if err = console.Loop(); err != nil {
+		log.Println(err)
+	}
+	log.Println("Terminating...")
 }
--- monitor/console_ui.go
+++ monitor/console_ui.go
@@ -1,1 +1,100 @@
 package monitor
+
+import (
+	"fmt"
+	"github.com/jroimartin/gocui"
+	"github.com/nsf/termbox-go"
+)
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("status", 0, 0, maxX-1, 2); err != nil {
+		if err != gocui.ErrorUnkView {
+			return err
+		}
+		fmt.Fprintln(v, "Waiting for initial status.")
+	}
+	if v, err := g.SetView("log", 0, 2, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrorUnkView {
+			return err
+		}
+		v.Autoscroll = true
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.Quit
+}
+
+type Console struct {
+	g      *gocui.Gui
+	status chan string
+	log    chan string
+}
+
+func NewConsole(status chan string, logEntry chan string) (*Console, error) {
+	g := gocui.NewGui()
+	if err := g.Init(); err != nil {
+		return nil, err
+	}
+	g.SetLayout(layout)
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		g.Close()
+		return nil, err
+	}
+	g.Flush()
+	return &Console{
+		g:      g,
+		status: status,
+		log:    logEntry,
+	}, nil
+}
+
+func (c *Console) Loop() error {
+	go func() {
+		v, err := c.g.View("status")
+		if err != nil {
+			return
+		}
+		for status := range c.status {
+			v.Clear()
+			fmt.Fprintln(v, status)
+			c.g.Flush()
+		}
+		fmt.Fprintln(v, "Status channel closed.")
+		c.g.Flush()
+	}()
+
+	go func() {
+		v, err := c.g.View("log")
+		if err != nil {
+			return
+		}
+		for line := range c.log {
+			fmt.Fprintln(v, line)
+			c.g.Flush()
+		}
+		fmt.Fprintln(v, "No more log entries.")
+		c.g.Flush()
+	}()
+
+	if err := c.g.SetCurrentView("log"); err != nil {
+		return err
+	}
+
+	if err := c.g.MainLoop(); err != gocui.Quit {
+		return err
+	}
+	return nil
+}
+
+func (c *Console) Interrupt() {
+	termbox.Interrupt()
+}
+
+func (c *Console) Cleanup() {
+	c.g.Close()
+	close(c.status)
+	close(c.log)
+}
