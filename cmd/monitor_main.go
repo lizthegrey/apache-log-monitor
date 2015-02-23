23edb67f2b00a9597f16bc9ba487201067a1b4dc
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2015-02-23 09:30:01 -0500

    Implement status line.

--- cmd/monitor_main.go
+++ cmd/monitor_main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"flag"
 	"github.com/lizthegrey/apache-log-monitor/monitor"
+
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -12,7 +15,10 @@ var filename = flag.String("file", "/var/log/apache2/access.log", "The filename 
 var pollIntervalMs = flag.Int64("poll_interval_ms", 250, "The number of milliseconds to wait between tail polling attempts.")
 
 var bucketSizeS = flag.Int64("bucket_size_s", 1, "The number of seconds of granularity for data collection.")
-var statsEvalS = flag.Int64("stats_eval_s", 10, "The number of seconds between computation of current statistics.")
+
+var statsEvalS = flag.Int64("stats_eval_s", 1, "The number of seconds between computation of current statistics.")
+var statsWindowS = flag.Int64("stats_window_s", 10, "The number of seconds for the sliding window for QPS alerts.")
+
 var alertEvalS = flag.Int64("alert_eval_s", 5, "The number of seconds between evaluation of alert conditions.")
 var highTrafficThresholdQps = flag.Int64("high_traffic_threshold_qps", 1, "The QPS value above which we will generate an alert.")
 var lowTrafficThresholdQps = flag.Int64("low_traffic_threshold_qps", 0, "The QPS value below which we will generate an alert.")
@@ -43,34 +49,87 @@ func main() {
 	// Define our statistics objects, and set them to rotate on a granular basis.
 	// Traffic alerting needs to expire buckets every bucketSizeS seconds,
 	// and keep a total number of buckets corresponding to the window.
-	trafficStats := monitor.NewRing(int(*trafficWindowS / *bucketSizeS))
+	trafficAlertStats := monitor.NewRing(int(*trafficWindowS / *bucketSizeS))
+	consoleStats := monitor.NewRing(int(*statsWindowS / *bucketSizeS))
+	pathStats := monitor.NewRing(int(*statsWindowS / *bucketSizeS))
+
 	go func() {
 		for {
 			time.Sleep(time.Duration(*bucketSizeS) * time.Second)
-			trafficStats.Rotate()
+
+			trafficAlertStats.Rotate()
+			consoleStats.Rotate()
+			pathStats.Rotate()
 		}
 	}()
 
 	// Invoke the log parser.
 	go func() {
 		for line := range lines {
-			// TODO(lizf): put back result once I have a use for it.
-			_, err := monitor.LogParse(line)
+			result, err := monitor.LogParse(line)
 			if err == nil {
-				// TODO(lizf): Send the result object to all registered statistics modules,
-				//             then add extra indirection layer for stats modules to write to console/status.
-
-				// Unconditionally increment the traffic stats mapping.
-				trafficStats.Mutate(func(s *monitor.Stat) { s.Map["all"] += 1 })
+				// Unconditionally increment the traffic stats mapping for each received query.
+				trafficAlertStats.Mutate(func(s *monitor.Stat) { s.Map["all"] += 1 })
 
 				// We want to record: QPS, bytes/sec, non-200s/sec, popular URL paths over status interval
 				// (cue "what do you want to monitor about a webserver?")
 				// We also want to track the scalar value of average processing delay between log entry arriving and us processing.
+				consoleStats.Mutate(func(s *monitor.Stat) {
+					s.Map["allReqs"] += 1
+					s.Map["allBytes"] += result.Size
+					if result.Status != 200 {
+						s.Map["non200s"] += 1
+					}
+					s.Map["logLatencyMs"] += int64(time.Since(result.Timestamp) / time.Millisecond)
+				})
+				path := strings.SplitN(result.Url.Path, "/", 3)
+				if len(path) >= 2 {
+					pathStats.Mutate(func(s *monitor.Stat) { s.Map[path[1]] += 1 })
+				}
 			} else {
 				consoleLog <- err.Error()
 			}
 		}
 		terminate <- true
+	}()
+
+	// Invoke the console stats loop.
+	go func() {
+		for {
+			consoleStats.Mtx.Lock()
+			count := float64(consoleStats.Sum.Map["allReqs"])
+			bytes := float64(consoleStats.Sum.Map["allBytes"])
+			non200s := float64(consoleStats.Sum.Map["non200s"])
+			latencyAvgMs := float64(consoleStats.Sum.Map["logLatencyMs"]) / count
+			consoleStats.Mtx.Unlock()
+
+			pathStats.Mtx.Lock()
+			var highestVal int64 = 0
+			var highestPath string = ""
+			for path, hits := range pathStats.Sum.Map {
+				if hits > highestVal {
+					highestVal = hits
+					highestPath = fmt.Sprintf("/%s", path)
+				}
+			}
+			pathStats.Mtx.Unlock()
+
+			// Construct line and send to status display.
+			window := float64(*statsWindowS)
+			basic := fmt.Sprintf("QPS %.1f, Bytes/sec %.1f, Errs/sec %.1f, ErrRatio %.2f, ", count/window, bytes/window, non200s/window, non200s/count)
+			var popular string
+			if highestVal > 0 {
+				popular = fmt.Sprintf("Popular %s (%d queries over %.fs window), ", highestPath, highestVal, window)
+			} else {
+				popular = fmt.Sprintf("Popular - (no traffic over %.fs window), ", window)
+			}
+			delay := fmt.Sprintf("Delay(ms) %.f", latencyAvgMs)
+
+			line := fmt.Sprint(basic, popular, delay)
+			consoleStatus <- line
+
+			time.Sleep(time.Duration(*statsEvalS) * time.Second)
+		}
 	}()
 
 	// Invoke the alert evaluation loop.
@@ -85,10 +144,10 @@ func main() {
 		}
 		for {
 			time.Sleep(time.Duration(*alertEvalS) * time.Second)
-			if msg := high.Evaluate(trafficStats.SumLookup("all")); msg != nil {
+			if msg := high.Evaluate(trafficAlertStats.SumLookup("all")); msg != nil {
 				consoleLog <- *msg
 			}
-			if msg := low.Evaluate(trafficStats.SumLookup("all")); msg != nil {
+			if msg := low.Evaluate(trafficAlertStats.SumLookup("all")); msg != nil {
 				consoleLog <- *msg
 			}
 		}
