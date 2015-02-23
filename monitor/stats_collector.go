9b6abe415ba235524497e9a5378818b9b324d1b0
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2015-02-22 23:58:39 -0500

    added ring-based stats collection.

--- monitor/stats_collector.go
+++ monitor/stats_collector.go
@@ -1,3 +1,82 @@
 package monitor
 
-// only retain data for maximum lifetime required to generate statistics for that bucketing scheme.
+import (
+	"container/ring"
+	"sync"
+	"log"
+)
+
+// Our goal is to retain data only for the maximum lifetime required to
+// generate statistics for that bucketing scheme.
+//
+// We are maintain cumulative statistics for the entire ring buffer as well
+// as statistics for the current bucket. That way, when this bucket reaches
+// the end of the buffer and needs to be expired, we can subtract its stats
+// from the cumulative stats and insert a new empty bucket into the ring.
+
+type Stat struct {
+	Map map[string]int64
+}
+
+func NewEmptyStat() *Stat {
+	val := make(map[string]int64)
+	return &Stat{val}
+}
+
+func (minuend *Stat) Subtract(subtrahend *Stat) {
+	for key, sValue := range subtrahend.Map {
+		if mValue, ok := minuend.Map[key]; ok {
+			if difference := mValue - sValue; difference != 0 {
+				minuend.Map[key] = difference
+			} else {
+				// Prune 0 entries from map.
+				delete(minuend.Map, key)
+			}
+		} else {
+			log.Panicf("Could not find key %s in minuend map.", key)
+		}
+	}
+}
+
+type RingBufferStats struct {
+	Sum *Stat
+	buckets *ring.Ring
+	mtx sync.Mutex
+}
+
+// Given a size and a zeroed instance of a stats object, populate the ring.
+func NewRing(size int) RingBufferStats {
+	r := ring.New(size)
+	// Initialize the ring with all zero elements.
+	for i := 0; i < size; i++ {
+		// Make a pointer to a new stats bucket.
+		r.Value = NewEmptyStat()
+		r = r.Next()
+	}
+	return RingBufferStats{
+		Sum: NewEmptyStat(),
+		buckets: r,
+	}
+}
+
+func (r *RingBufferStats) Rotate() {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	// Remove the expiring bucket's stats from the running totals.
+	r.Sum.Subtract(r.buckets.Next().Value.(*Stat))
+
+	// Move the current pointer up, and empty the bucket.
+	r.buckets = r.buckets.Next()
+	r.buckets.Value = NewEmptyStat()
+}
+
+// Takes required locks, then applies the mutation operation to running total
+// and to the current bucket.
+func (r *RingBufferStats) Mutate(f func(*Stat)) {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	f(r.Sum)
+	f(r.buckets.Value.(*Stat))
+}
---
+++ monitor/stats_collector_test.go
@@ -0,0 +1,43 @@
+package monitor
+
+import "testing"
+
+func TestRingBufferStats(t *testing.T) {
+	cases := []struct {
+		rotateBefore bool
+		increment   int64
+		sum         int64
+	}{
+		{false, 0, 0}, // {0, {0, 0, 0}}
+		{false, 1, 1}, // {1, {1, 0, 0}}
+		{false, 2, 3}, // {3, {3, 0, 0}}
+		{true, 1, 4},  // {4, {1, 3, 0}}
+		{false, 5, 9}, // {9, {6, 3, 0}}
+		{true, 2, 11}, // {11, {2, 6, 3}}
+		{true, 0, 8},  // {8, {0, 2, 6}}
+		{false, 1, 9}, // {9, {1, 2, 6}}
+		{true, 0, 3},  // {7, {0, 1, 2}}
+		{true, 0, 1},  // {1, {0, 0, 1}}
+		{true, 0, 0},  // {0, {0, 0, 0}}
+	}
+
+	r := NewRing(3)
+
+	// Repeatedly add to the ring, making sure rotation works as intended.
+	for _, c := range cases {
+		if c.rotateBefore {
+			r.Rotate()
+		}
+		r.Mutate(func (v *Stat) {
+			if sum := v.Map["test"] + c.increment; sum != 0 {
+				v.Map["test"] = sum
+			}
+		})
+		if total := r.Sum.Map["test"]; c.sum != total {
+			t.Errorf("Sum - Expected: %d, Got: %d", c.sum, total)
+		}
+	}
+	if _, found := r.Sum.Map["test"]; found {
+		t.Errorf("Leaking memory - 0 values should be deleted.")
+	}
+}
