b0dceeba174881af1f9413a66f5946e7104713c7
Liz Fong-Jones <elizabeth@ugcs.caltech.edu>
2015-02-22 21:35:43 -0500

    Add tests for log parsing code.

--- monitor/log_parse.go
+++ monitor/log_parse.go
@@ -16,9 +16,37 @@ type LogLine struct {
 	Timestamp time.Time
 	IpAddr    string
 	Method    string
-	Url       *url.URL
+	Url       url.URL
 	Status    int
 	Size      int64
+}
+
+func (l *LogLine) Equal(other *LogLine) error {
+	if l == nil || other == nil {
+		if l == nil && other == nil {
+			return nil
+		}
+		return fmt.Errorf("Existence: %s != %s", l, other)
+	}
+	if !l.Timestamp.Equal(other.Timestamp) {
+		return fmt.Errorf("Timestamp: %s != %s", l.Timestamp, other.Timestamp)
+	}
+	if l.IpAddr != other.IpAddr {
+		return fmt.Errorf("IpAddr: %s != %s", l.IpAddr, other.IpAddr)
+	}
+	if l.Method != other.Method {
+		return fmt.Errorf("Method: %s != %s", l.Method, other.Method)
+	}
+	if l.Url != other.Url {
+		return fmt.Errorf("Url: %s != %s", l.Url, other.Url)
+	}
+	if l.Status != other.Status {
+		return fmt.Errorf("Status: %s != %s", l.Status, other.Status)
+	}
+	if l.Size != other.Size {
+		return fmt.Errorf("Size: %s != %s", l.Size, other.Size)
+	}
+	return nil
 }
 
 func LogParse(line string) (*LogLine, error) {
@@ -52,7 +80,7 @@ func LogParse(line string) (*LogLine, error) {
 		Timestamp: stamp,
 		IpAddr:    results[1],
 		Method:    results[3],
-		Url:       uri,
+		Url:       *uri,
 		Status:    status,
 		Size:      size,
 	}, nil
--- monitor/log_parse_test.go
+++ monitor/log_parse_test.go
@@ -1,7 +1,38 @@
 package monitor
 
-import "testing"
+import (
+	"testing"
+
+	"errors"
+	"net/url"
+	"time"
+)
 
 func TestParsing(t *testing.T) {
-	//cases := []struct{}
+	cases := []struct {
+		in   string
+		want *LogLine
+		err  error
+	}{
+		{
+			"127.0.0.1 - frank [11/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200 2326",
+			&LogLine{
+				Timestamp: time.Date(2000, 10, 11, 13, 55, 36, 0, time.FixedZone("-0700", -7*60*60)),
+				IpAddr:    "127.0.0.1",
+				Method:    "GET",
+				Url:       url.URL{Path: "/apache_pb.gif"},
+				Status:    200,
+				Size:      2326,
+			},
+			nil,
+		},
+		{"bogus", nil, errors.New("Failed to parse line: bogus")},
+	}
+	for _, c := range cases {
+		got, err := LogParse(c.in)
+
+		if lineDiff := got.Equal(c.want); lineDiff != nil || !((err == nil && c.err == nil) || err.Error() == c.err.Error()) {
+			t.Errorf("LogParse(%q)\nResult: (%q, %t)\nWanted: (%q, %t)\nDiff: %s", c.in, got, err, c.want, c.err, lineDiff)
+		}
+	}
 }
